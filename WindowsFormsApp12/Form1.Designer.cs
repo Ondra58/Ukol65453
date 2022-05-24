186df1ae943a319edb67949a90aaa38e271e691e
Ondra58 <97514734+Ondra58@users.noreply.github.com>
2022-05-24 18:46:10 +0200

    Add files via upload

---
+++ WindowsFormsApp12/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace WindowsFormsApp12
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Vyžaduje se proměnná návrháře.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Uvolněte všechny používané prostředky.
+        /// </summary>
+        /// <param name="disposing">hodnota true, když by se měl spravovaný prostředek odstranit; jinak false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kód generovaný Návrhářem Windows Form
+
+        /// <summary>
+        /// Metoda vyžadovaná pro podporu Návrháře - neupravovat
+        /// obsah této metody v editoru kódu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.DarkOrange;
+            this.button1.Font = new System.Drawing.Font("Impact", 12.25F);
+            this.button1.Location = new System.Drawing.Point(114, 166);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(128, 56);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Zjisti, zda je číslo prvočíslo";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(118, 124);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 245);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Impact", 25.25F);
+            this.label2.Location = new System.Drawing.Point(47, 33);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(262, 42);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Hrátky s prvočísly";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(62, 291);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(110, 20);
+            this.textBox1.TabIndex = 6;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(178, 291);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(100, 108);
+            this.listBox1.TabIndex = 7;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.DarkOrange;
+            this.button2.Font = new System.Drawing.Font("Impact", 12.25F);
+            this.button2.Location = new System.Drawing.Point(114, 424);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(128, 56);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Přesunout prvočísla";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(62, 317);
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(110, 20);
+            this.numericUpDown2.TabIndex = 9;
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.DarkOrange;
+            this.button3.Font = new System.Drawing.Font("Impact", 12.25F);
+            this.button3.Location = new System.Drawing.Point(62, 343);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(110, 55);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "Vygenerovat čísla";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gold;
+            this.ClientSize = new System.Drawing.Size(359, 500);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.button1);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ WindowsFormsApp12/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp12
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        bool JePrvocislo(int vstup)
+        {
+            bool prvocislo = false;
+            if (vstup == 2 || vstup == 3)
+            {
+                prvocislo = true;
+            }
+            else if (vstup % 2 == 0 && vstup != 2 || vstup == 1)
+            {
+                prvocislo = false;
+            }
+            else
+            {
+                prvocislo = true;
+                for (int i = 3; i < Math.Sqrt(vstup) && prvocislo != false; i += 2)
+                {
+                    if (vstup % i == 0)
+                    {
+                        prvocislo = false;
+                    }
+                    else
+                    {
+                        prvocislo = true;
+                    }
+                }
+            }
+            return prvocislo;
+        }
+        int[] pole;
+        void Prepis(TextBox textBox1, ListBox listBox1)
+        {
+            listBox1.Items.Clear();
+            foreach (int cislo in pole)
+            {
+                if (JePrvocislo(cislo))
+                {
+                    listBox1.Items.Add(cislo);
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int vstup = (int)numericUpDown1.Value;
+            if (JePrvocislo(vstup) == true)
+            {
+                label1.Text = "Číslo je prvočíslo";
+            }
+            else 
+            {
+                label1.Text = "Číslo není prvočíslo";
+            }
+        }
+        Random rng = new Random();
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            pole = new int[(int)numericUpDown2.Value];
+            for (int i = 0; i < (int)numericUpDown2.Value; i++)
+            {
+                int random = rng.Next(2, 16);
+                pole[i] = random;
+                textBox1.Text += (random + "; ");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Prepis(textBox1, listBox1);
+        }
+    }
+}
